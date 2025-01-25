c1bfab84924643bc590341afb6030e941d42c262
Eniola Aderounmu <date1272@gmail.com>
2025-01-25 02:22:45 +0100

    Jan 25

---
+++ build/client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./ui/Header";
+import Footer from "./ui/Footer";
+import Container from "./ui/Container";
+// import { Container } from "./ui/Container";
+
+function App() {
+
+  return (
+    <main>
+      <Container>
+        <p>Ecommerce demo</p>
+      </Container>
+    
+    </main>
+  );
+}
+
+export default App;
---
+++ build/client/src/main.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { createBrowserRouter, Outlet, BrowserRouter as Router, RouterProvider } from 'react-router-dom';
+import "./index.css";
+import Layout from './ui/Layout';
+import Product from './pages/Product';
+import Category from './pages/Category';
+import Profile from './pages/Profile';
+import Cart from './pages/Cart';
+import Favourite from './pages/Favourite';
+import Orders from './pages/Orders';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
+import Notfound from './pages/Notfound';
+
+const RouterLayout = () => {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
+}
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <RouterLayout />,
+  children: [
+    {
+      path: "/",
+      element: <App />,
+    },
+    
+    {
+      path: "/product",
+      element: <Product />,
+    },
+    
+    {
+      path: "/product/:id",
+      element: <Product />,
+    },
+    
+    {
+      path: "/category",
+      element: <Category />,
+    },
+    
+    {
+      path: "/category/:id",
+      element: <Category />,
+    },
+    
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+    
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+    
+    {
+      path: "/favourite",
+      element: <Favourite />,
+    },
+    
+    {
+      path: "/orders",
+      element: <Orders />,
+    },
+    
+    {
+      path: "/success",
+      element: <Success />,
+    },
+    
+    {
+      path: "/cancel",
+      element: <Cancel />,
+    },
+    
+    {
+      path: "*",
+      element: <Notfound />,
+    },
+  ],
+}]);
+
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
+
+
