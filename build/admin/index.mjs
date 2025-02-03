fa8bad4a7b269fd7005568ec3047c0c53664fd52
Eniola Aderounmu <date1272@gmail.com>
2025-02-03 04:35:40 +0100

    update

--- build/admin/index.mjs
+++ build/admin/index.mjs
@@ -1,11 +1,21 @@
 import express from 'express'
 const app = express()
-const port = 8000
+import "dotenv/config";
+import cors from 'cors';
+import { fileURLPath } from 'url';
+
+
+const port = process.env.PORT || 8000;
+app.use(cors);
+app.use(express.json())
+
+const __filename = fileURLPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
